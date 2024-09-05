581dfafff0921a31e4915c2cfc063c3b38e8ad5c
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-09-05 23:27:04 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <form>
+        <h1>Form in React</h1>
+      </form>
+    </>
+  )
+}
+
+export default App
